d54066ee43b9b71668287a1b12202f57352f00ed
RawxHB <rawxhabbo@gmail.com>
2022-01-21 09:23:41 -0500

    Update

--- Comet-Server/src/main/java/com/cometproject/server/game/commands/staff/GiveRankCommand.java
+++ Comet-Server/src/main/java/com/cometproject/server/game/commands/staff/GiveRankCommand.java
@@ -6,6 +6,7 @@ import com.cometproject.server.game.commands.ChatCommand;
 import com.cometproject.server.game.players.PlayerManager;
 import com.cometproject.server.network.NetworkManager;
 import com.cometproject.server.network.messages.outgoing.notification.AlertMessageComposer;
+import com.cometproject.server.network.messages.outgoing.notification.MotdNotificationMessageComposer;
 import com.cometproject.server.network.sessions.Session;
 import com.cometproject.server.storage.queries.player.PlayerDao;
 import org.apache.commons.lang.StringUtils;
@@ -16,10 +17,36 @@ import java.util.List;
 public class GiveRankCommand extends ChatCommand {
     private String logDesc = "";
 
+
     @Override
     public void execute(Session client, String[] params) {
         if (params.length < 2 || !StringUtils.isNumeric(params[1]))
-            return;
+                client.send(new MotdNotificationMessageComposer(
+                        "Aca tienes la lista de los rangos disponibles en el hotel, usa ;giverank <ID> <username> command.\n\n" +
+                                "- [1] Usuario\n" +
+                                "- [2] VIP\n" +
+                                "- [3] Equipo Creativo\n" +
+                                "- [4] Deejay\n" +
+                                "- [5] Intermediario\n" +
+                                "- [6] Master Trade\n" +
+                                "- [7] Publicista\n" +
+                                "- [8] Jefe De Publicidad\n" +
+                                "- [9] Marketing\n" +
+                                "- [10] Room Builder\n" +
+                                "- [11] Helper\n" +
+                                "- [12] Embajador\n" +
+                                "- [13] Game Master\n" +
+                                "- [14] Colaboración\n" +
+                                "- [15] Coordinador\n" +
+                                "- [16] Moderador\n" +
+                                "- [17] Supervisor\n" +
+                                "- [18] Inversionista (oculto)\n" +
+                                "- [19] Economista (oculto)\n" +
+                                "- [20] Manager\n" +
+                                "- [21] Ceo\n" +
+                                "- [22] Desarrollador\n" +
+                                "- [23] Fundador"
+                ));
         String username = params[0];
         int rank = Integer.parseInt(params[1]);
 
@@ -44,7 +71,7 @@ public class GiveRankCommand extends ChatCommand {
         PlayerDao.updateRank(user.getPlayer().getId(), rank);
 
         client.getPlayer().sendNotif("success", Locale.getOrDefault("command.giverank.success", "Rank set succesfully!"));
-        user.getPlayer().sendNotif("Rank updated", Locale.getOrDefault("command.giverank.received", "Your rank was set to " + rank + ". Please reload client"));
+        user.getPlayer().sendNotif("Rank updated", Locale.getOrDefault("command.giverank.received", "Your rank was set to %r. Please reload client").replace("%r", Integer.toString(rank)));
 
         try {
             DiscordClient dcClient = new DiscordClient("https://discord.com/api/webhooks/933726180195045396/YapXa9ru4V2g91E7jPdAD9Y0sQXTTN9diINcWt8O96Ug9DsvRzrwMhX3hgbGsa3h_0zD");
