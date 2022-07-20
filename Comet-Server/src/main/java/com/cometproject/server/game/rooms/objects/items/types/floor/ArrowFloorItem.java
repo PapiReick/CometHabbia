76f13d1c14de5209e47129db07f2738bed266c8a
Laux <Luifer041@gmail.com>
2022-07-20 10:10:10 +0200

    Fix Toggle ArrowFloorItem

--- Comet-Server/src/main/java/com/cometproject/server/game/rooms/objects/items/types/floor/ArrowFloorItem.java
+++ Comet-Server/src/main/java/com/cometproject/server/game/rooms/objects/items/types/floor/ArrowFloorItem.java
@@ -1,8 +1,10 @@
 package com.cometproject.server.game.rooms.objects.items.types.floor;
 
+import com.cometproject.api.game.quests.QuestType;
 import com.cometproject.api.game.rooms.objects.data.RoomItemData;
 import com.cometproject.api.game.utilities.Position;
 import com.cometproject.server.game.rooms.objects.entities.RoomEntity;
+import com.cometproject.server.game.rooms.objects.entities.types.PlayerEntity;
 import com.cometproject.server.game.rooms.objects.items.RoomItemFloor;
 import com.cometproject.server.game.rooms.types.Room;
 import com.cometproject.server.utilities.collections.ConcurrentHashSet;
@@ -18,6 +20,33 @@ public class ArrowFloorItem extends RoomItemFloor {
 
     public ArrowFloorItem (RoomItemData itemData, Room room) {
         super(itemData, room);
+    }
+
+    @Override
+    public boolean onInteract(RoomEntity entity, int requestData, boolean isWiredTrigger) {
+        if (!isWiredTrigger) {
+            if (!(entity instanceof PlayerEntity)) {
+                return false;
+            }
+
+            PlayerEntity pEntity = (PlayerEntity) entity;
+
+            if (this.getDefinition().requiresRights()) {
+                if (!pEntity.getRoom().getRights().hasRights(pEntity.getPlayerId()) && !pEntity.getPlayer().getPermissions().getRank().roomFullControl()) {
+                    return false;
+                }
+            }
+
+            if (pEntity.getPlayer().getId() == this.getRoom().getData().getOwnerId()) {
+                pEntity.getPlayer().getQuests().progressQuest(QuestType.FURNI_SWITCH);
+            }
+        }
+
+        this.toggleInteract(true);
+        this.sendUpdate();
+
+        this.saveData();
+        return true;
     }
 
     @Override
